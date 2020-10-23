15e1198be75865a8ef3ccb10ec41be89912e8293
gksrbans <37265014+gksrbans@users.noreply.github.com>
2020-10-23 11:10:38 +0900

    Update [카카오 인턴] 키패드 누르기.py

--- [카카오 인턴] 키패드 누르기.py
+++ [카카오 인턴] 키패드 누르기.py
@@ -92,3 +92,55 @@ def solution(numbers, hand):
                 pass
 
     return answer
+
+
+##################################################################
+
+# 다른 사람이 풀이...
+# 이렇게 깔끔하게 짤수가 있구나!
+
+# numbers = [7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2]
+# hand = "left"
+
+# number_dict = {1:(0,0),2:(1,0),3:(2,0),
+#                4:(0,1),5:(1,1),6:(2,1),
+#                7:(0,2),8:(1,2),9:(2,2),
+#                '*':(0,3),0:(1,3),'#':(2,3),}
+
+# left_hand = [1,4,7]
+# right_hand = [3,6,9]
+# l_hand = '*'
+# r_hand = '#'
+# answer = ''
+
+# for i in numbers:
+#     if i in left_hand:
+#         answer += 'L'
+#         l_hand = i
+#     elif i in right_hand:
+#         answer += 'R'
+#         r_hand = i
+#     else:
+#         temp = number_dict[i]
+#         lPos = number_dict[l_hand]
+#         rPos = number_dict[r_hand]
+
+#         l_range = abs(temp[0] - lPos[0]) + abs(temp[1] - lPos[1])
+#         r_range = abs(temp[0] - rPos[0]) + abs(temp[1] - rPos[1])
+
+#         if l_range < r_range:
+#             answer += 'L'
+#             l_hand = i
+#         elif l_range > r_range:
+#             answer += 'R'
+#             r_hand = i
+#         else:
+#             if hand == 'left':
+#                 answer += 'L'
+#                 l_hand = i
+#             else:
+#                 answer += 'R'
+#                 r_hand = i
+# print(answer)
+
+##################################################################
